72c64291d88e4b656ce9aa0d995720be3a76674f
Nikhil-Zawar <157366906+Nikhil-Zawar@users.noreply.github.com>
2024-01-25 16:32:04 +0530

    Update app.py
    Added libraries and from statements. Added the register route.

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from dateutil.relativedelta import relativedelta
 from datetime import date 
 import pandas as pd
 from jugaad_data.nse import stock_df
+
+from market import app, db
+from flask import render_template, redirect, url_for
+from market.models import Item, User
+from market.forms import RegisterForm
+from datetime import date
+from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save
+import pandas as pd
 # import cufflinks as cf
 
 app = Flask(__name__)
@@ -53,6 +61,23 @@ def show_graph():
         return render_template('graph.html', plot=fig.to_html(), stock_symbol=stock_symbol, duration=duration)
 
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET','POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit(): #means when clicked on the submit button, then check inside the scope of if
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
+
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
