68aa5a85da994607d6bea7292d6ebc92f0fb422c
Nikhil-Zawar <157366906+Nikhil-Zawar@users.noreply.github.com>
2024-01-25 16:33:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from market import app, db
+from flask import render_template, redirect, url_for
+from market.models import Item, User
+from market.forms import RegisterForm
+from datetime import date
+from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save
+import pandas as pd
