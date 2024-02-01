2e19308a79458f3ab8e001cdf6d9fcea8408d516
Nikhil-Zawar <157366906+Nikhil-Zawar@users.noreply.github.com>
2024-02-01 23:50:32 +0530

    Add files via upload

--- market/app.py
+++ market/app.py
@@ -8,18 +8,18 @@ from jugaad_data.nse import bhavcopy_save, stock_df, index_df
 import pandas as pd
 import requests
 import csv
+import atexit
+import os
 from jugaad_data.nse import NSELive
 n = NSELive()
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with your actual secret key
+app.secret_key = 'your_secret_key' 
 NEWS_API_KEY = '737401ce7fb44b42b952c278fb01ebf0'
-# Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
@@ -32,9 +32,7 @@ class User(db.Model):
 class UserData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    # Add fields for user-specific data, e.g., user_dashboard_content, etc.
 
-# Initialize Database within Application Context
 with app.app_context():
     db.create_all()
 
@@ -53,7 +51,6 @@ def about_page():
 def contact_page():
     return render_template('contact.html')
 
-# ------------Working login and registration page from the starter code ------------------------------------
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
     if request.method == 'POST':
@@ -94,7 +91,7 @@ def login_page():
         return redirect(url_for('dashboard'))
     else:
         flash('Invalid username or password')
-        return redirect(url_for('just_login'))
+        return redirect(url_for('home_page'))
 
 first_write = False
 toppers_found = False
@@ -133,11 +130,8 @@ def make_toppers(file_path):
                 else:
                     write_dict_to_csv([q], 'toppers_data.csv', write_header=False)
                 # print(q)
-# Read the CSV file into a Pandas DataFrame
     df = pd.read_csv('toppers_data.csv')
-# Sort the DataFrame based on the 'pChange' column in descending order
     df_sorted = df.sort_values(by='pChange', ascending=False)
-# Write the sorted DataFrame back to the CSV file
     df_sorted.to_csv('toppers_data.csv', index=False)
 
 @app.route('/dashboard')
@@ -145,35 +139,34 @@ def dashboard():
     global toppers_found
     if 'user_id' in session:
         user = User.query.get(session['user_id'])
-        user_data = user.user_data  # Modify this based on your data structure
+        user_data = user.user_data 
         if not toppers_found:
             make_toppers('nifty50list.csv')
             toppers_found = True
-        # Read top gainers and top losers data from the CSV file
+
         topers_df = pd.read_csv('toppers_data.csv')
         top_gainers = topers_df.iloc[:3].to_dict(orient='records')
         top_losers = topers_df.iloc[-3:].to_dict(orient='records')
         index_data = index_df(symbol="NIFTY 50", from_date=date(2023,1,1),to_date=date(2024,1,30))
         fig = px.line(index_data, x='HistoricalDate', y='CLOSE', title='NIFTY 50 Index')
+        
         fig.update_layout(
             template='plotly_dark',
-            paper_bgcolor='rgba(0, 0, 0, 0)',  # Make background transparent
-            plot_bgcolor='rgba(0, 0, 0, 0)',  # Make plot area transparent
-            # showgrid=False,  # Hide grid lines
+            paper_bgcolor='rgba(0, 0, 0, 0)', 
+            plot_bgcolor='rgba(0, 0, 0, 0)',  
+
             xaxis=dict(
                 tickmode='linear',
                 tick0=0,
-                dtick=30 * 24 * 3600 * 1000,  # 1 month in milliseconds
-                tickformat='%d %b %Y',
+                dtick=30 * 24 * 3600 * 1000, 
+                tickformat=' %b %Y',
             ),
             yaxis=dict(
                 tickmode='linear',
                 tick0=0,
-                dtick=400,  # Increase ticks every 2000 units on the y-axis
+                dtick=400,
             ),
             margin=dict(l=0, r=0, t=0, b=0),
-            # xaxis_showgrid=False,  # Hide x-axis grid lines
-            # yaxis_showgrid=False,  # Hide y-axis grid lines
             yaxis_gridcolor='rgba(0, 0, 0, 0)',
             xaxis_gridcolor='rgba(0, 0, 0, 0)',
         )
@@ -189,11 +182,9 @@ def dashboard():
 @app.route('/graph', methods=['POST', 'GET'])
 def show_graph():
     if request.method == 'POST':
-        # stock_symbols = request.form['stock_symbol'].split(',')  # Split symbols by comma
         duration = request.form['duration']
         stock_symbols_input = request.form['stock_symbol']
         stock_symbols = [symbol.strip() for symbol in stock_symbols_input.split(',')] 
-        # additional_stock = request.form.get('additional_stock')
         stock_info=request.form['stock_data']
         today = date.today()
         if duration == '1w':
@@ -207,7 +198,7 @@ def show_graph():
         elif duration == '5y':
             back = today - relativedelta(years=5)
         else:
-            back = today - relativedelta(days=7)  # Default to 1 day
+            back = today - relativedelta(days=7)
 
         combined_data = pd.DataFrame({'DATE': pd.date_range(start=back, end=today)})
 
@@ -215,16 +206,11 @@ def show_graph():
             stock_data = stock_df(symbol=symbol, from_date=back, to_date=today, series="EQ")
             stock_data = stock_data.sort_values(by='DATE', ascending=True)
             
-            # Merge the data into the combined DataFrame using DATE as the key
             combined_data = pd.merge(combined_data, stock_data[['DATE', stock_info]].rename(columns={stock_info: symbol}),how='left', on='DATE')
-        # combined_data=combined_data.interpolate
-        # combined_data['DATE'] = pd.to_datetime(combined_data['DATE'])  # Ensure 'DATE' column is in datetime format
         combined_data = combined_data.set_index('DATE').interpolate().reset_index()
         fig = px.line(combined_data, x='DATE', y=combined_data.columns[1:], title=f'Stock {stock_info} ({duration.capitalize()})')
-        
         fig.update_traces(hovertemplate='Date: %{x}<br>' + f'{stock_info}:' + ' %{y:.2f} ')
         fig.update_xaxes(title_text='Date', tickformat='%d %b %Y')
-        # fig.update_yaxes(title_text='Stock Price (INR)')
         if stock_info == "VOLUME":
             fig.update_yaxes(title_text='STOCK VOLUME')
         elif stock_info == "VALUE":
@@ -318,13 +304,12 @@ def get_news():
     params = {
         'apiKey': NEWS_API_KEY,
         'category': 'business',
-        'country': 'in',  # Adjust country code as needed
+        'country': 'in',
     }
 
     response = requests.get(endpoint, params=params)
     news_data = response.json()
 
-    # Extract relevant data from the response, adjust as needed
     articles = news_data.get('articles', [])
 
     return jsonify(articles)
@@ -353,65 +338,31 @@ def user_details():
     else:
         flash('Please log in to view user details.')
         return redirect(url_for('just_login'))
-
-
-# -------------Login page and registration page using forms ----------------
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
-
-class RegisterForm(FlaskForm):
-    username = StringField(label='', validators=[Length(min=2, max=30), DataRequired()])
-    email_address = StringField(label='', validators=[DataRequired()])
-    password1 = PasswordField(label='', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='', validators=[ DataRequired()])
-    submit = SubmitField(label='Create Account')
-
-class LoginForm(FlaskForm):
-    username = StringField(label='Enter your Username / Email Address', validators=[Length(min=2,max=30), DataRequired()])
-    password = PasswordField(label='Enter your password', validators=[Length(min=6), DataRequired()])
-    submit = SubmitField(label='Login')
-
-@app.route('/register2', methods=['GET','POST'])
-def register_page2():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        email_address = form.email_address.data
-        password = form.password1.data
-        new_user = User(username,password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        flash('Registration successful! Please login.')
-        return redirect(url_for('login_page2'))
-
-    return render_template('register2.html', form=form)
-
-def check_password(plaintext_password, stored_password):
-    return plaintext_password == stored_password
-
-@app.route('/login2')
-def login_page2():
-    form=LoginForm()
-    username = form.username.data
-    password = form.password.data
-    check_user = User.query.filter_by(username).first()
-    new_password = check_user.password_hash
-    if check_user and check_password(new_password,password):
-        session['user_id'] = check_user.id
-        session['username'] = check_user.username
-        return redirect(url_for('dashboard_page'))
-    else:
-        flash('Invalid username or password')
-        return redirect(url_for('index'))
-    # return render_template('login.html', form=form)
+    
+    
+@app.route('/get_stock_info', methods=['GET'])
+def get_stock_info():
+    symbol = request.args.get('symbol')
+    q = n.stock_quote(symbol)
+    p = q['info']
+    q = q['priceInfo']
+    newd= {'symbol' : symbol,
+        'name' : p['companyName'],
+        'series' : p['industry'],
+        'lastPrice' : q['lastPrice'],
+        'pChange': round(q['pChange'], 3),
+        'change': round(q['change'], 3),
+        'intradaylow' : q['intraDayHighLow']['min'],
+        'intradayhigh' : q['intraDayHighLow']['max'],
+        'week52high' : q['weekHighLow']['max'],
+        'week52low' : q['weekHighLow']['min'],
+        }
+    print(newd)
+    return jsonify(newd)
 
 @app.route('/dashboard2')
 def dashboard_page():
     return render_template('dashboard.html')
-
 
 @app.route('/filter', methods=["GET","POST"])
 def filterpage():
@@ -474,11 +425,9 @@ def filterpage():
     else:
         flash('Please log in to access the dashboard.')
         return redirect(url_for('just_login'))
-    
-import atexit
-import os
 
-# The file path you want to delete
+
+
 file_pa = "toppers_data.csv"
 def get_csv_name():
     today = datetime.now()
@@ -493,7 +442,6 @@ def get_csv_name():
 
 market_file = get_csv_name()
 
-# Function to delete the file
 def cleanup():
     try:
         os.remove(file_pa)
@@ -509,7 +457,6 @@ def cleanup1():
         print(f"File '{market_file}' not found.")
 
 
-# Register the cleanup function to be called at program exit
 atexit.register(cleanup)
 atexit.register(cleanup1)
 
